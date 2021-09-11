8dfdd5b54c750c9e572aaa9e6f1028696210e63c
Luca Corrias <luca.corrias@hotmail.it>
2021-09-11 12:25:20 +0200

    Esercizio N.Primi

---
+++ PrimeNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Scrivi un numero:");
+        int numero = sc.nextInt();
+        int []
+    }
+}
+[]+i
